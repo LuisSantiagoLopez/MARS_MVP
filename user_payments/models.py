c0f77e6e73e5879fe80e95303620c170392799e1
LuisSantiagoLopez <luis.santiago.l.sanchez@gmail.com>
2024-02-21 18:52:31 -0600

    resolucion de problema con stripe no listo

--- user_payments/models.py
+++ user_payments/models.py
@@ -7,7 +7,7 @@ from django.db.models.signals import post_save
 class UserPayments(models.Model):
   app_user = models.ForeignKey(User, on_delete=models.CASCADE)
   payment_bool = models.BooleanField(default=False)
-  stripe_subscription_id = models.CharField(max_length=500)
+  stripe_checkout_id = models.CharField(max_length=500)
   stripe_customer_id = models.CharField(max_length=255, null=True, blank=True) 
 
 
