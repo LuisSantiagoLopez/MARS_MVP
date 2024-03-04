1314252aea67474415f3f3ac26561b77aef00948
LuisSantiagoLopez <luis.santiago.l.sanchez@gmail.com>
2024-03-03 19:33:02 -0600

    fixed static roots

--- django_chatbot/settings.py
+++ django_chatbot/settings.py
@@ -135,11 +135,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/home/django/MARS_MVP/staticfiles/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'),]
 
-MEDIA_URL = '/home/django/MARS_MVP/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
