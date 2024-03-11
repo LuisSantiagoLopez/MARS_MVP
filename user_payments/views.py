3b2e58d20919909b3ca7f2680d4e9659f055d0d4
LuisSantiagoLopez <luis.santiago.l.sanchez@gmail.com>
2024-03-11 15:44:53 -0600

    stripe

--- user_payments/views.py
+++ user_payments/views.py
@@ -118,6 +118,7 @@ def stripe_webhook(request):
    subscription_id = subscription.get("subscription")
    customer_id = subscription.get("customer")
 
+   logger.debug(f"INFO EVENT: {subscription}")
    logger.debug(f"INFO EVENT: Type {event_type}, checkout id {checkout_id}, customer {customer_id}, subscription id {subscription_id}")
 
    user_payment = UserPayments.objects.get(checkout_id=checkout_id)
